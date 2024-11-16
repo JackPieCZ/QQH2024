2b5aae96d6f31acb8f527a0237355b9d4a3d4309
Jakub Kolar <jakubkolar211@gmail.com>
2024-11-16 14:24:42 +0100

    Templates for win prop strategies

---
+++ src/strats/win_prob_sviga.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
+def calculate_win_probs_sviga(summary, opp, games_inc, players_inc):
+    """Calculates win probabilities for home and away team.
+
+        Args:
+            opp (pd.Dataframe): Betting opportunities with columns ['Season', 'Date', 'HID', 'AID', 'N', 'POFF', 'OddsH', 'OddsA', 'BetH', 'BetA'].
+
+        Returns:
+            tuple(float, float): Probability of home team winning, probability of away team winning.
+        """
+    current_season = opp['Season']
+    match_date = opp['Date']
+    home_ID = opp['HID']
+    away_ID = opp['AID']
+    neutral_ground = opp['N']
+    playoff_game = opp['POFF']
+    oddsH = opp['OddsH']
+    oddsA = opp['OddsA']
+    return 0.5, 0.5
+
+
+def calculate_win_probs_sviga2(summary, opp, games_inc, players_inc):
+    """Calculates win probabilities for home and away team.
+
+        Args:
+            opp (pd.Dataframe): Betting opportunities with columns ['Season', 'Date', 'HID', 'AID', 'N', 'POFF', 'OddsH', 'OddsA', 'BetH', 'BetA'].
+
+        Returns:
+            tuple(float, float): Probability of home team winning, probability of away team winning.
+        """
+    current_season = opp['Season']
+    match_date = opp['Date']
+    home_ID = opp['HID']
+    away_ID = opp['AID']
+    neutral_ground = opp['N']
+    playoff_game = opp['POFF']
+    oddsH = opp['OddsH']
+    oddsA = opp['OddsA']
+    return 0.5, 0.5
